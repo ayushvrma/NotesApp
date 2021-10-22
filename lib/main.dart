6258280872fc80ea50792a80b27e4a3ea37252a0
Ayush <ayush8402@gmail.com>
2021-10-22 22:18:08 +0530

    added add task screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'models/tasks.dart';
+import 'screens/new_task.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.from(
           colorScheme: const ColorScheme.highContrastDark(
               background: Color(0xff1D2025),
-              primary: Color(0xff102A43),
+              primary: Color(0xff9FB3C8),
               secondary: Color(0xffADE69B))),
       home: HomePage(),
     );
@@ -54,11 +56,19 @@ class _HomePageState extends State<HomePage> {
           itemCount: taskBox.length,
           itemBuilder: (BuildContext ctx, index) {
             return Container(
-              alignment: Alignment.center,
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(8),
               child: Column(
                 children: [
-                  Text(taskBox.getAt(index)!.title),
-                  Text(taskBox.getAt(index)!.descp),
+                  Text(
+                    taskBox.getAt(index)!.title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(
+                    taskBox.getAt(index)!.descp,
+                    style:
+                        TextStyle(fontWeight: FontWeight.normal, fontSize: 14),
+                  ),
                 ],
               ),
               decoration: BoxDecoration(
@@ -71,56 +81,58 @@ class _HomePageState extends State<HomePage> {
         // When the user presses the button, show an alert dialog containing
         // the text that the user has entered into the text field.
         onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Column(
-                  children: [
-                    TextField(
-                      controller: myController,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter a Task',
-                      ),
-                    ),
-                    TextField(
-                      controller: myController2,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter a Description',
-                      ),
-                    ),
-                  ],
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () {
-                      myController.text = '';
-                      myController2.text = '';
-                      Navigator.pop(context, 'Cancel');
-                      setState(() {});
-                    },
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      taskBox.add(Task(myController.text, myController2.text));
-                      myController.text = '';
-                      myController2.text = '';
-                      Navigator.pop(context, 'OK');
-                      setState(() {});
-                    },
-                    child: const Text('OK'),
-                  ),
-                ],
-              );
-            },
-          );
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => NewTask()));
+          // showDialog(
+          //   context: context,
+          //   builder: (context) {
+          //     return AlertDialog(
+          //       // Retrieve the text the that user has entered by using the
+          //       // TextEditingController.
+          //       content: Column(
+          //         children: [
+          //           TextField(
+          //             controller: myController,
+          //             autofocus: true,
+          //             decoration: InputDecoration(
+          //               border: OutlineInputBorder(),
+          //               labelText: 'Enter a Task',
+          //             ),
+          //           ),
+          //           TextField(
+          //             controller: myController2,
+          //             autofocus: true,
+          //             decoration: InputDecoration(
+          //               border: OutlineInputBorder(),
+          //               labelText: 'Enter a Description',
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //       actions: <Widget>[
+          //         TextButton(
+          //           onPressed: () {
+          //             myController.text = '';
+          //             myController2.text = '';
+          //             Navigator.pop(context, 'Cancel');
+          //             setState(() {});
+          //           },
+          //           child: const Text('Cancel'),
+          //         ),
+          //         TextButton(
+          //           onPressed: () {
+          //             taskBox.add(Task(myController.text, myController2.text));
+          //             myController.text = '';
+          //             myController2.text = '';
+          //             Navigator.pop(context, 'OK');
+          //             setState(() {});
+          //           },
+          //           child: const Text('OK'),
+          //         ),
+          //       ],
+          //     );
+          //   },
+          // );
         },
         tooltip: 'Entered the task',
         child: const Icon(Icons.add),
---
+++ lib/screens/new_task.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NewTask extends StatefulWidget {
+  @override
+  _NewTaskState createState() => _NewTaskState();
+}
+
+class _NewTaskState extends State<NewTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back)),
+      ),
+    );
+  }
+}

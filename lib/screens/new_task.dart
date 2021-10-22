33242cfc13c5f7a8e28494866861caf6287c6b2c
Ayush <ayush8402@gmail.com>
2021-10-22 22:45:04 +0530

    added custom btm nav bar

--- lib/screens/new_task.dart
+++ lib/screens/new_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 
 class NewTask extends StatefulWidget {
   @override
@@ -16,6 +17,19 @@ class _NewTaskState extends State<NewTask> {
             },
             child: Icon(Icons.arrow_back)),
       ),
+      bottomNavigationBar: CustomNavigationBar(
+          iconSize: 25.0,
+          selectedColor: Colors.white,
+          strokeColor: Colors.white,
+          unSelectedColor: Colors.grey[600],
+          backgroundColor: Colors.black,
+          blurEffect: true,
+          opacity: 0.8,
+          items: [
+            CustomNavigationBarItem(icon: Icon(Icons.palette)),
+            CustomNavigationBarItem(icon: Icon(Icons.more))
+          ]),
+      body: Container(alignment: Alignment.center, child: Text('lets go')),
     );
   }
 }
